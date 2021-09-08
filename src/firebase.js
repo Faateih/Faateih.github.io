e175eccc6fdb564609c3fcc87d62da8a92f7a427
geekycoder26@gmail.com <Hassaan00@gmail.com>
2021-09-08 13:32:05 +0500

    initialize project

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcjwLPMAT5sfo61-Cz-GpZgUILBNf4j9A",
+  authDomain: "cbsolsite.firebaseapp.com",
+  projectId: "cbsolsite",
+  storageBucket: "cbsolsite.appspot.com",
+  messagingSenderId: "305838610519",
+  appId: "1:305838610519:web:26a9ba6128a9acece0eb12",
+  measurementId: "G-R4XHJHCLTT",
+};
+
+export const db = firebase.initializeApp(firebaseConfig);
