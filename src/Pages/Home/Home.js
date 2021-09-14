300771f84139014c06f00e85a95702071d2c4732
Usmanhaiderr <usmanhb12@gmail.com>
2021-09-14 19:01:00 +0500

    Cotact us page design completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
 import Partner from "../Parnter/Partner";
+import Queries from "../Queries/Queries";
 import Questions from "../Questions/Questions";
 import Headd from "../Services/Headd";
- 
-
+import mail from '../../assets/umairbhai.jpg';
+import mobil from '../../assets/phone.svg';
+import fb from '../../assets/frazbhai.jpg';
+import { FiSend } from 'react-icons/fi';
 const Home = () => {
   return (
     <div>
       <Headd />
       <Partner />
       <Questions />
+      <Queries iconmail={<FiSend/>}
+       queryhead='Have a project in mind? Need help with an ongoing one?' querysub="We'll be happy to talk!" button='Send Message' 
+       mailpic={mail} mailname='Umair Ali' maildetail='Cheif Executive Officer, CbSol' mobile={mobil}
+       mailpicf={fb} mailnamef='Fraz Khan' maildetailf='Project Manager, CbSol'
+       />
     </div>
   );
 };
