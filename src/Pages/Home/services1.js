d3c2e8b3b0ed7dfa5b504fab5c0b11ffb3133207
unknown <faatiehsaaqib366@gmail.com>
2021-09-20 14:25:42 +0500

    updates

---
+++ src/Pages/Home/services1.js
@@ -0,0 +1,7 @@
+import React from "react";
+import TechnologiesMain from "../../components/technologiesmain";
+
+const Services1 = () => {
+  return <TechnologiesMain text="services" />;
+};
+export default Services1;
